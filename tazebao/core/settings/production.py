a8bdd65f627b9649dc3386d6fadcd5b5e70f4e5c
abidibo <abidibo@gmail.com>
2016-10-10 10:36:25 +0200

    go ssl

--- tazebao/core/settings/production.py
+++ tazebao/core/settings/production.py
@@ -9,6 +9,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['tazebao.sqrt64.it', ]
 
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
+SESSION_COOKIE_SECURE = True
+
 STATIC_ROOT = '/home/tazebao/www/tazebao/static/'
 MEDIA_ROOT = '/home/tazebao/www/tazebao/media'
 
@@ -23,7 +28,7 @@ CKEDITOR_CONFIGS['default']['contentsCss'] = [
     STATIC_URL + 'core/src/css/ckeditor.css']
 
 LOGGING['handlers']['file']['filename'] = here('..', '..', '..', '..', os.path.join('logs', 'debug.log')) # noqa
-LOGGING['handlers']['celery_logger']['filename'] = here('..', '..', '..', '..', os.path.join('logs', 'celery.log'))
+LOGGING['handlers']['celery_logger']['filename'] = here('..', '..', '..', '..', os.path.join('logs', 'celery.log')) # noqa
 
 # CELERY
 CELERY_ACCEPT_CONTENT = ['json']
