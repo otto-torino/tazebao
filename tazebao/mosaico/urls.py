d76323607ceed292550943c3acbeec950359b6f1
abidibo <abidibo@gmail.com>
2016-12-01 13:28:04 +0100

    fixes 404 warnings - social icons in template editor

--- tazebao/mosaico/urls.py
+++ tazebao/mosaico/urls.py
@@ -1,9 +1,31 @@
+import os
+
 from django.conf.urls import url
+from django.views.static import serve
+from django.conf import settings
 
 from .views import index, editor, upload, download, image, template, template_content # noqa
 
 app_name = 'mosaico'
+
 urlpatterns = [
+    url(
+        r'^img/social_def/(?P<path>.*)$',
+        serve,
+        {
+            'document_root': os.path.join(
+                settings.STATIC_ROOT,
+                'mosaico',
+                'templates',
+                'versafix-1',
+                'img',
+                'social_def'
+            )
+        }
+    ),
+]
+
+urlpatterns += [
     url(r'^$', index, name="index"),
     url(r'^editor.html$', editor, name="editor"),
     url(r'^img/$', image),
