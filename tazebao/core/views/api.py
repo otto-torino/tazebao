155c3cf77b75d8d8f86a8bbe4a00c18f158780a1
abidibo <abidibo@gmail.com>
2019-12-03 10:48:06 +0100

    adds client info to whoami api

--- tazebao/core/views/api.py
+++ tazebao/core/views/api.py
@@ -14,10 +14,17 @@ class WhoAmI(APIView):
             return Response({'description': 'not authenticated'}, status=401)
             response = {}
         else:
+            client = None
+            if request.user.client:
+                client = {
+                    'idKey': request.user.client.id_key,
+                    'secretKey': request.user.client.secret_key,
+                }
             response = {
                 'userId': request.user.id,
                 'userName': request.user.username,
                 'userEmail': request.user.email,
-                'isSuperUser': request.user.is_superuser
+                'isSuperUser': request.user.is_superuser,
+                'client': client,
             }
         return Response(response)
