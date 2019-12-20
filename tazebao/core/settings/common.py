92f731aaafc00da6ec1ac56cd35745d5ae2df0d5
abidibo <abidibo@gmail.com>
2019-12-20 17:13:09 +0100

    allow all cors

--- tazebao/core/settings/common.py
+++ tazebao/core/settings/common.py
@@ -269,14 +269,15 @@ JWT_AUTH = {
     'JWT_EXPIRATION_DELTA': datetime.timedelta(minutes=60),
     'JWT_ALLOW_REFRESH': True,
 }
-CORS_ORIGIN_WHITELIST = (
-    'http://127.0.0.1',
-    'https://app.tazebao.sqrt64.it',
-    'http://localhost',
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'localhost',
-)
+# CORS_ORIGIN_WHITELIST = (
+#     'http://127.0.0.1',
+#     'https://app.tazebao.sqrt64.it',
+#     'http://localhost',
+#     'http://localhost:3000',
+#     'http://localhost:3001',
+#     'localhost',
+# )
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = (
     'cache-control',
